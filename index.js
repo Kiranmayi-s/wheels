924ab055dd8b7df1641543c2c3c26ea800a410e8
Kiranmayi-s <83873537+Kiranmayi-s@users.noreply.github.com>
2021-09-15 14:29:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-function fun(card,img){
+function Zoom(card,img){
+for(let i=0;i<3;i++){
+        document.getElementsByClassName("card")[i].style.height =  "250px";
+        document.getElementsByClassName("card")[i].style.width = "250px";
+        document.getElementsByClassName("image")[i].style.width = "150px";
+        document.getElementsByClassName("image")[i].style.height = "150px";
+        document.getElementsByClassName("image")[i].style.marginLeft = "50px";
+    } 
     if(document.getElementById(card).style.height === "350px"){
-    document.getElementById(card).style.height = "250px"
-    document.getElementById(card).style.width = "250px";
-    document.getElementById(img).style.height = "150px";
-    document.getElementById(img).style.width = "150px";
-    document.getElementById(img).style.marginLeft = "50px";
-    }else{
-    document.getElementById(card).style.height = "350px";
-    document.getElementById(card).style.width = "250px";
-    document.getElementById(img).style.height = "200px";
-    document.getElementById(img).style.width = "200px";
-    document.getElementById(img).style.marginLeft = "25px";
-    }
+        document.getElementById(card).style.height = "250px"
+        document.getElementById(card).style.width = "250px";
+        document.getElementById(img).style.height = "150px";
+        document.getElementById(img).style.width = "150px";
+        document.getElementById(img).style.marginLeft = "50px";
+        }else{
+        document.getElementById(card).style.height = "350px";
+        document.getElementById(card).style.width = "250px";
+        document.getElementById(img).style.height = "200px";
+        document.getElementById(img).style.width = "200px";
+        document.getElementById(img).style.marginLeft = "25px";
+        }
 }
 function slidefun(){
         document.getElementById("img1").src = "wgu.png";
         document.getElementById("img2").src = "wjr.png";
         document.getElementById("img3").src = "wp6.png";
-        document.getElementById("content1").innerHTML = "Tires can include natural<b> rubber, synthetic rubber, steel, nylon, silica (derived from sand), polyester, carbon</b> etc."
-        document.getElementById("content2").innerHTML = "A modern tire on a passenger car will contain up to <b>25 components and also as many as 12 different</b>."
-        document.getElementById("content3").innerHTML = "Steel wire is used in the tire belts and beads, and the<b> plies for truck tires</b>. The belts under the tread serve to <b>stiffen</b>.";
+        document.getElementById("content1").innerText = "Tires can include natural rubber, synthetic rubber, steel, nylon, silica (derived from sand), polyester, carbon etc."
+        document.getElementById("content2").innerText = "A modern tire on a passenger car will contain up to 25 components and also as many as 12 different."
+        document.getElementById("content3").innerText = "Steel wire is used in the tire belts and beads, and the plies for truck tires. The belts under the tread serve to stiffen.";
         document.getElementById("revslide").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqQA8DCzSOlHz3KKV7vEB6_-DcAdoAioBnSQju2vwz5B9TutYyKQtOSOVVJwusB6NDgw0&usqp=CAU";
         document.getElementById("revslide").style.height = "30px";
         document.getElementById("revslide").style.width = "35px";
